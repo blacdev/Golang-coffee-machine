f06e255363ba5441529d6fef7bc5d9301b4cb8a5
blacdev <stevenowoborode@gmail.com>
2022-04-13 16:02:51 +0100

    cleaned up the files

--- main.go
+++ main.go
@@ -31,11 +31,10 @@ func Greeting() {
 func ingredientCalculator() {
 	var numOfCups int
 	var rwater, rcoffee, rmilk int
-	var water, coffee, milk, machinewater, machinecoffee, machinemilk, machinecups int
+	var machinewater, machinecoffee, machinemilk, machinecups int
 
-	water, milk, coffee, machinewater, machinecoffee, machinemilk, machinecups = machineIngredient()
+	_, _, _, machinewater, machinecoffee, machinemilk, machinecups = machineIngredient()
 
-	F.Println("machineIngredient", water, milk, coffee, machinewater, machinecoffee, machinemilk, machinecups)
 	F.Println("Write how many cups of coffee you will need:")
 	F.Scan(&numOfCups)
 
@@ -43,7 +42,6 @@ func ingredientCalculator() {
 	rmilk = (actualMilk * numOfCups)
 	rcoffee = (actualCoffee * numOfCups)
 
-	F.Printf("response %d %d %d \n", rwater, rmilk, rcoffee)
 	if rmilk == 0 && rwater == 0 && rcoffee == 0 && machinemilk == 0 && machinecoffee == 0 && machinewater == 0 && numOfCups == 0 {
 		F.Println("Yes, I can make that amount of coffee.")
 	} else {
@@ -51,12 +49,10 @@ func ingredientCalculator() {
 		fmilk := round(float64(machinemilk) / float64(rmilk))
 		fcoffee := round(float64(machinecoffee) / float64(rcoffee))
 
-		F.Printf("makeable cups %d %d %d \n", fwater, fmilk, fcoffee)
-
 		array := []int{fwater, fmilk, fcoffee}
 		sort.Ints(array)
 		cup := array[0]
-		F.Println("array index one", cup)
+
 		//F.Printf("For %v cups of coffee you will need:\n", numOfCups)
 		//F.Printf("%v ml of water\n", rwater)
 		//F.Printf("%v ml of milk\n", rcoffee)
@@ -77,10 +73,7 @@ func ingredientCalculator() {
 		} else if machinecups > numOfCups {
 			// subtract cups from cup
 			// the answer is the amount of cups that can be made more than the amount  requested for
-			F.Println(machinecups)
-			F.Println(cup)
 			remainderofMachinecups := machinecups - numOfCups
-			F.Println(remainderofMachinecups)
 			F.Printf("Yes, I can make that amount of coffee (and even %d more than that)\n", remainderofMachinecups)
 		} else {
 			F.Printf("No, I can make only %d cups of coffee\n", machinecups)
@@ -101,17 +94,13 @@ func machineIngredient() (water, milk, coffee, machinewater, machinecoffee, mach
 	array := []int{water, milk, coffee}
 	sort.Ints(array)
 	machinecups = array[0]
-	F.Println(array)
-	F.Println(machinecups)
 	return
 }
 
 func round(x float64) (D int) {
-	F.Println("value of x", x)
 	d := math.Ceil(x)
 	s := math.Floor(x)
 	g := d - x
-	F.Println("weting dey sup", g)
 	if g <= 0.1 {
 		D = int(d)
 	} else {
