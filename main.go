95174d56e0878e2b6ae47468f0f987ac3baa4ee3
blacdev <stevenowoborode@gmail.com>
2022-04-13 16:42:01 +0100

    added comments to make it easier to read the code

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"sort"
 )
 
+// these are the actual values required in a making a cup of coffee
 const (
 	actualWater  int = 200
 	actualCoffee int = 15
@@ -18,6 +19,7 @@ func main() {
 	ingredientCalculator()
 }
 
+// First thing to show when the machine is turned on
 func Greeting() {
 	F.Println("Starting to make a coffee")
 	F.Println("Grinding coffee beans")
@@ -33,22 +35,28 @@ func ingredientCalculator() {
 	var rwater, rcoffee, rmilk int
 	var machinewater, machinecoffee, machinemilk, machinecups int
 
+	// varaibles returned from machine ingredient function
 	_, _, _, machinewater, machinecoffee, machinemilk, machinecups = machineIngredient()
 
 	F.Println("Write how many cups of coffee you will need:")
+	// take in put from the user to know how many cups they need
 	F.Scan(&numOfCups)
 
+	// calculate how much ingredient is required to make the number of cups requested for
 	rwater = (actualWater * numOfCups)
 	rmilk = (actualMilk * numOfCups)
 	rcoffee = (actualCoffee * numOfCups)
 
+	// check if all values return zero
 	if rmilk == 0 && rwater == 0 && rcoffee == 0 && machinemilk == 0 && machinecoffee == 0 && machinewater == 0 && numOfCups == 0 {
 		F.Println("Yes, I can make that amount of coffee.")
 	} else {
+		// round up or down the the values afeter dividing the ingredients in the machine required amount of ingredient
 		fwater := round(float64(machinewater) / float64(rwater))
 		fmilk := round(float64(machinemilk) / float64(rmilk))
 		fcoffee := round(float64(machinecoffee) / float64(rcoffee))
 
+		// sort the the values to know the smallest value which would be the amount of cups of coffee that can be made
 		array := []int{fwater, fmilk, fcoffee}
 		sort.Ints(array)
 		cup := array[0]
@@ -58,8 +66,11 @@ func ingredientCalculator() {
 		//F.Printf("%v ml of milk\n", rcoffee)
 		//F.Printf("%v g of coffee beans\n", rmilk)
 
+		// check if the number of cups the machine can make is less or equal to the numbercups that can be made based on the cups requested for
 		if machinecups <= cup && numOfCups > cup {
 			cup = cup - machinecups
+
+			// check if cup is less than 0
 			if cup < 0 {
 				cup = 0
 				F.Printf("No, I can only make %d cups of coffee\n", cup)
@@ -67,9 +78,12 @@ func ingredientCalculator() {
 				F.Printf("No, I can only make %d cups of coffee\n", cup)
 			}
 
+			// check if all cups are of the same value
 		} else if machinecups == cup && numOfCups == machinecups {
 			// can give the exact amount of cups requested for
 			F.Println("Yes, I can make that amount of coffee ")
+
+			// check if machine cups is greater than number of cups requested for
 		} else if machinecups > numOfCups {
 			// subtract cups from cup
 			// the answer is the amount of cups that can be made more than the amount  requested for
@@ -88,9 +102,11 @@ func machineIngredient() (water, milk, coffee, machinewater, machinecoffee, mach
 	F.Println("Write how many grams of coffee beans the machine has:")
 	F.Scan(&machinecoffee)
 
+	// divide the amount of ingredients in the machine by the actual amount of ingredient neede to make a cup of coffee
 	water = machinewater / actualWater    // Tells us how many cups of coffee can be made from what is in the machine
 	milk = machinemilk / actualMilk       // Tells us how many cups of coffee can be made from what is in the machine
 	coffee = machinecoffee / actualCoffee // Tells us how many cups of coffee can be made from what is in the machine
+	// sort the the values to know the smallest value which would be the amount of cups of coffee that can be made
 	array := []int{water, milk, coffee}
 	sort.Ints(array)
 	machinecups = array[0]
@@ -98,10 +114,17 @@ func machineIngredient() (water, milk, coffee, machinewater, machinecoffee, mach
 }
 
 func round(x float64) (D int) {
+	// round up x
 	d := math.Ceil(x)
+
+	// round down x
 	s := math.Floor(x)
+
+	// subtract x from d
 	g := d - x
-	if g <= 0.1 {
+
+	// check if the value is less than or equal to .004%
+	if g <= 0.004 {
 		D = int(d)
 	} else {
 		D = int(s)
